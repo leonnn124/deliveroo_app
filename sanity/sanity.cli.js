80aa136a0fd9b9dc02d3393b934163131a3edbe5
leonnn124 <s0976144676s@gmail.com>
2023-01-29 15:44:06 +0800

    import sanity.io

---
+++ sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'bu3125ez',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemas/dish.js
@@ -0,0 +1,31 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'dish',
+  title: 'Dish',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Dish Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'short_description',
+      type: 'string',
+      title: 'Short Description',
+      validation: (Rule) => Rule.max(200),
+    }),
+    defineField({
+      name: 'price',
+      type: 'number',
+      title: 'Price of Dish',
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+      title: 'Dish Image',
+    }),
+  ],
+})
